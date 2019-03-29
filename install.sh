4a9cdf89ecfb287cbecca70ac0e29e2319fc81fe
qinweijunlin <qinweijunlin@qq.com>
2019-03-29 14:29:48 +0800

    发布

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ readonly REPO='cygwin'
 readonly url="https://github.com/yzaj/${REPO}.git"
 drive='c'
 
+if [[ -d "/d" ]]; then
+  drive='d'
+fi
+
 readonly drive
+readonly ownerdir="/${drive}/yzaj"
 readonly repodir="/${drive}/yzaj/${REPO}"
 
 git clone --depth 1 "${url}" "${repodir}"
